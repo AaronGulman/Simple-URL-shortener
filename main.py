1bee814d1e0a5ca2670aad320581372f0797320f
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-13 20:37:46 -0500

    Added URL shortening interface components to the tkinter application

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ root = tkinter.Tk()
 root.title("URL Shortener")
 root.geometry("600x300")
 
+longUrl_label = tkinter.Label(root, text = "Enter your URL: ")
+longUrl_entry = tkinter.Entry(root)
+shortUrl_label = tkinter.Label(root, text ="Shortened URL: ")
+shortUrl_entry = tkinter.Entry(root)
+shorten_button = tkinter.Button(root,text = "Shortern URL")
+
+
 
 root.mainloop()
